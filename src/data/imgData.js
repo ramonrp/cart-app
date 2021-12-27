8b0bd5f95ddff521250d346ab63099ce929351ce
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-12-27 17:29:50 +0100

    mock Imgs

---
+++ src/data/imgData.js
@@ -0,0 +1,60 @@
+const imgData = [
+  {
+    id: "a",
+    pictureUrl:
+      "https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/594a1ced5bafe85dfd3c9869/gato-romano_0.jpg",
+    title: "one cat",
+    category: "cat",
+  },
+  {
+    id: "b",
+    pictureUrl:
+      "https://static3.lasprovincias.es/www/multimedia/202112/12/media/cortadas/gatos-kb2-U160232243326NVC-1248x770@Las%20Provincias.jpg",
+    title: "one cat",
+    category: "cat",
+  },
+  {
+    id: "c",
+    pictureUrl:
+      "https://images.ecestaticos.com/FVdcvD11qPRi-JWDH3USTiXDmeQ=/0x0:2120x1414/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F47b%2F328%2F963%2F47b3289639713b8e80c8d682d219fba7.jpg",
+    title: "one cat",
+    category: "cat",
+  },
+  {
+    id: "d",
+    pictureUrl:
+      "https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/59c4f5655bafe82c692a7052/gato-marron_0.jpg",
+    title: "one cat",
+    category: "cat",
+  },
+  {
+    id: "e",
+    pictureUrl:
+      "http://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg",
+    title: "one dog",
+    category: "dog",
+  },
+  {
+    id: "f",
+    pictureUrl:
+      "https://www.ecestaticos.com/imagestatic/clipping/797/767/79776773aab795837282c7d4947abaf7/por-que-nos-parece-que-los-perros-sonrien-una-historia-de-30-000-anos.jpg?mtime=1622645511",
+    title: "one dog",
+    category: "dog",
+  },
+  {
+    id: "g",
+    pictureUrl:
+      "https://media.traveler.es/photos/613760adcb06ad0f20e11980/master/w_1600%2Cc_limit/202931.jpg",
+    title: "one dog",
+    category: "dog",
+  },
+  {
+    id: "h",
+    pictureUrl:
+      "https://www.petdarling.com/wp-content/uploads/2020/11/razas-de-perros.jpg",
+    title: "one dog",
+    category: "dog",
+  },
+];
+
+export { imgData };
