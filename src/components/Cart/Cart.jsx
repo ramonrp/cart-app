bfae1c8b3e5b12cef27eee4955470f4f39fa3a5c
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-12-29 11:48:04 +0100

    add styles

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 import { useCart } from "../../context/CartContext";
 import CartItem from "../Cart/CartItem/CartItem";
 import { imgData } from "../../data/imgData";
+import styles from "./cart.module.css";
 const Cart = ({ onDismiss }) => {
   const { cart, handleEmptyCart } = useCart();
   let filteredCart = imgData.filter((item) => cart.includes(item.id));
-
   const CartItems = filteredCart.map((item) => (
     <CartItem key={item.id} {...item} />
   ));
@@ -14,12 +14,16 @@ const Cart = ({ onDismiss }) => {
     <div>
       <h2>Cart</h2>
       {CartItems}
-      {cart.length > 0 && <button onClick={handleEmptyCart}>Delete All</button>}
-      {cart.length > 0 && (
-        <Link onClick={onDismiss} to="/checkout">
-          Checkout
-        </Link>
-      )}
+      <div className={styles.buttonGroup}>
+        {cart.length > 0 && (
+          <button onClick={handleEmptyCart}>Delete All</button>
+        )}
+        {cart.length > 0 && (
+          <Link onClick={onDismiss} to="/checkout">
+            Checkout
+          </Link>
+        )}
+      </div>
     </div>
   );
 };
