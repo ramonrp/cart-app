3da68710558b3c34eff45ced22d0b8b87912e17b
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-12-29 11:39:57 +0100

    feature: show checkout items

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,8 +1,24 @@
 import { useCart } from "../../context/CartContext";
-
+import { imgData } from "../../data/imgData";
+import CheckoutItem from "./CheckoutItem";
+import styles from "./checkout.module.css";
 const CheckOut = () => {
   const { cart } = useCart();
-  return <h1>Checkout!!</h1>;
+  let filteredCart = imgData.filter((item) => cart.includes(item.id));
+  const total = filteredCart.reduce((acc, item) => {
+    return acc + item.price;
+  }, 0);
+  const CheckoutItems = filteredCart.map((item) => (
+    <CheckoutItem key={item.id} {...item} />
+  ));
+  return (
+    <div>
+      <ol>{CheckoutItems}</ol>
+      <p className={styles.total}>
+        Total price <span>{total}€</span>
+      </p>
+    </div>
+  );
 };
 
 export default CheckOut;
