46067f6e7a0997aab87d19f0e2197de4486e038d
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-12-29 13:36:39 +0100

    add material NavBar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,27 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import styles from "./header.module.css";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
 const Header = () => {
+  const [tab, setTab] = useState(0);
+  console.dir(tab);
   return (
     <header>
       <h1>Animal Gallery Picture</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">All</Link>
-          </li>
-          <li>
-            <Link to="/cats">Cats</Link>
-          </li>
-          <li>
-            <Link to="/dogs">Dogs</Link>
-          </li>
-        </ul>
-      </nav>
+      <AppBar position="static">
+        <Tabs
+          value={tab}
+          onChange={(e, newValue) => setTab(newValue)}
+          aria-label="simple tabs example"
+        >
+          <Tab label="All" to="/" component={Link} />
+          <Tab label="Cats" to="/cats" component={Link} />
+          <Tab label="Dogs" to="/dogs" component={Link} />
+        </Tabs>
+      </AppBar>
     </header>
   );
 };
