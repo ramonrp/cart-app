f770e6c91057ccfe3c48ac774c7b0234dbef922a
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-12-29 09:49:29 +0100

    add dialog and styles

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
+import { DialogOverlay, DialogContent } from "@reach/dialog";
 import { CartProvider } from "../../context/CartContext";
 import Cart from "../Cart/Cart";
 import Gallery from "../Gallery/Gallery";
 import styles from "./main.module.css";
 const Main = () => {
+  const [showCart, setShowCart] = useState(false);
+  const close = () => setShowCart(false);
+  const open = () => setShowCart(true);
   return (
-    <main>
-      <CartProvider>
-        <Gallery />
-        <Cart />
-      </CartProvider>
-    </main>
+    <>
+      {!showCart && (
+        <button className={styles.buttonOpen} onClick={open}>
+          Open Cart
+        </button>
+      )}
+      <main>
+        <CartProvider>
+          <Gallery />
+          <DialogOverlay
+            className={styles.dialog}
+            isOpen={showCart}
+            onDismiss={close}
+          >
+            <DialogContent className={styles.content} aria-label="cart">
+              <Cart />
+            </DialogContent>
+          </DialogOverlay>
+        </CartProvider>
+      </main>
+    </>
   );
 };
 
